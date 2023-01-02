d5e710218a5be197f56896c30b40109915366ecb
mihalazmaeen <azmaeenmihal1@gmail.com>
2023-01-02 15:33:51 +0600

    admin edit image

--- resources/views/admin/admin_profile_edit.blade.php
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -1,5 +1,6 @@
 @extends('admin.admin_master')
 @section('admin')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <div class="container-full">
 
         <section class="content">
@@ -50,13 +51,13 @@
                                                 <div class="form-group">
                                                     <h5>Profile Photo <span class="text-danger">*</span></h5>
                                                     <div class="controls">
-                                                        <input type="file" name="profile_photo_path" class="form-control" required=""> <div class="help-block"></div></div>
+                                                        <input id="image" type="file" name="profile_photo_path" class="form-control" required=""> <div class="help-block"></div></div>
                                                 </div>
 
                                             </div>
 
                                             <div class="col-md-6">
-                                            <img src="{{(!empty($editData->profile_photo_path))? url('upload/admin_images/'.$editData->profile_photo_path): url('upload/no_image.jpg')}}" style="width: 100px;height: 100px;">
+                                            <img id="showimage" src="{{(!empty($editData->profile_photo_path))? url('upload/admin_images/'.$editData->profile_photo_path): url('upload/no_image.jpg')}}" style="width: 100px;height: 100px;"" >
                                             </div>
                                         </div>
                                         <div class="text-xs-right">
@@ -79,4 +80,21 @@
         </section>
 
     </div>
+
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('#image').change(function(e){
+                let reader=new FileReader();
+                reader.onload=function(e){
+                    $('#showimage').attr('src',e.target.result);
+                }
+                reader.readAsDataURL(e.target.files['0']);
+            })
+        })
+
+
+
+
+
+    </script>
 @endsection
