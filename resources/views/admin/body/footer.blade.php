a99e10930cc17d9c91e4d848e2d0e7b681dda542
mihalazmaeen <azmaeenmihal1@gmail.com>
2023-01-02 13:56:01 +0600

    admin template refactor

--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -2,12 +2,12 @@
     <div class="pull-right d-none d-sm-inline-block">
         <ul class="nav nav-primary nav-dotted nav-dot-separated justify-content-center justify-content-md-end">
             <li class="nav-item">
-                <a class="nav-link" href="javascript:void(0)">FAQ</a>
+                <a class="nav-link" href="">FAQ</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">Purchase Now</a>
             </li>
         </ul>
     </div>
-    &copy; 2020 <a href="#">Psd to Html Expert</a>. All Rights Reserved.
+    &copy; 2020 <a href="#">BShop</a>. All Rights Reserved.
 </footer>
