b6eafb166a89e1bf7897c08453597bb64891101f
mihalazmaeen <azmaeenmihal1@gmail.com>
2023-01-05 12:34:46 +0600

    user profile page design

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,27 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-           Hi..{{ Auth::user()->name }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        This is just home page 
+@extends('frontend.main_master')
+@section('content')
+<div class="body-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2"><br>
+                <img class="card-img-top" style="border-radius:50% " src="{{(!empty($editData->profile_photo_path))? url('upload/admin_images/'.$editData->profile_photo_path): url('upload/no_image.jpg')}}" height="100%" width="100%"><br>><br>
+                <ul class="list-group list-group-flush">
+                    <a href="#" class="btn btn-primary btn-sm btn-block">Home</a>
+                    <a href="#" class="btn btn-primary btn-sm btn-block">Profile update</a>
+                    <a href="#" class="btn btn-primary btn-sm btn-block">Change password</a>
+                    <a href="#" class="btn btn-danger btn-sm btn-block">Logout</a>
+                </ul>
+            </div>
+            <div class="col-md-2"></div>
+            <div class="col-md-6">
+                <div class="card">
+                    <h3 class="text-center">
+                        <span class="text-danger">Hello...</span><strong>{{Auth::user()->name}} </strong> Welcome to BShop !!!
+                    </h3>
+                </div>
+            </div>
+        </div>
     </div>
-</x-app-layout>
+</div>
+
+@endsection
