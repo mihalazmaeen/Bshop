eb6a0344ee98de18eec25262b6be53be3778963b
mihalazmaeen <azmaeenmihal1@gmail.com>
2023-01-14 13:58:19 +0600

    fetch subcategories, subsub categories on header with multi language

--- resources/views/frontend/body/header.blade.php
+++ resources/views/frontend/body/header.blade.php
@@ -153,7 +153,9 @@
                     <div class="navbar-collapse collapse" id="mc-horizontal-menu-collapse">
                         <div class="nav-outer">
                             <ul class="nav navbar-nav">
-                                <li class="active dropdown yamm-fw"> <a href="home.html" data-hover="dropdown" class="dropdown-toggle" data-toggle="dropdown">Home</a> </li>
+                                <li class="active dropdown yamm-fw"> <a href="home.html" data-hover="dropdown" class="dropdown-toggle" data-toggle="dropdown">
+                                        @if(session()->get('language')=='bengali') হোম @else Home @endif
+                                    </a> </li>
 {{--                                Get category Data--}}
                                 @php
                                 $categories=App\Models\Category::orderBy('category_name_en','ASC')->get();
@@ -162,7 +164,8 @@
 
 {{--                                Category Loop--}}
                                 @foreach($categories as $category)
-                                <li class="dropdown yamm mega-menu"> <a href="home.html" data-hover="dropdown" class="dropdown-toggle" data-toggle="dropdown">{{$category->category_name_en}}</a>
+                                <li class="dropdown yamm mega-menu"> <a href="home.html" data-hover="dropdown" class="dropdown-toggle" data-toggle="dropdown">
+                                        @if(session()->get('language')=='bengali') {{$category->category_name_bengali}} @else {{$category->category_name_en}} @endif </a>
                                     <ul class="dropdown-menu container">
                                         <li>
                                             <div class="yamm-content ">
@@ -174,7 +177,8 @@
 
                                                     @foreach($subcategories as $subcategory)
                                                     <div class="col-xs-12 col-sm-6 col-md-2 col-menu">
-                                                        <h2 class="title">{{$subcategory->subcategory_name_en}}</h2>
+                                                        <h2 class="title">
+                                                            @if(session()->get('language')=='bengali') {{$subcategory->subcategory_name_bengali}} @else {{$subcategory->subcategory_name_en}} @endif </h2>
 {{--                                                        Get Sub-SubCategory Data--}}
 
                                                         @php
@@ -183,7 +187,8 @@
                                                         @foreach($subsubcategories as $subsubcat)
 
                                                         <ul class="links">
-                                                            <li>{{$subsubcat->subsubcategory_name_en}}</li>
+                                                            <li><a href="">
+                                                                    @if(session()->get('language')=='bengali') {{$subsubcat->subsubcategory_name_bengali}} @else {{$subsubcat->subsubcategory_name_en}} @endif </a> </li>
 
                                                         </ul>
                                                         @endforeach
