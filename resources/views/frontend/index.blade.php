39da0f6916de8e136847642607cbf968aba48a63
mihalazmaeen <azmaeenmihal1@gmail.com>
2023-01-14 17:22:25 +0600

    fetch frontend slider

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -835,12 +835,13 @@
 
                 <div id="hero">
                     <div id="owl-main" class="owl-carousel owl-inner-nav owl-ui-sm">
-                        <div class="item" style="background-image: url({{asset('frontend/assets/images/sliders/01.jpg')}});">
+                        @foreach($sliders as $slider)
+                        <div class="item" style="background-image: url({{asset($slider->slider_image)}});">
                             <div class="container-fluid">
                                 <div class="caption bg-color vertical-center text-left">
-                                    <div class="slider-header fadeInDown-1">Top Brands</div>
-                                    <div class="big-text fadeInDown-1"> New Collections </div>
-                                    <div class="excerpt fadeInDown-2 hidden-xs"> <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span> </div>
+                                    <div class="slider-header fadeInDown-1">{{$slider->title}}</div>
+                                    <div class="big-text fadeInDown-1">{{$slider->description}}</div>
+
                                     <div class="button-holder fadeInDown-3"> <a href="index.php?page=single-product" class="btn-lg btn btn-uppercase btn-primary shop-now-button">Shop Now</a> </div>
                                 </div>
                                 <!-- /.caption -->
@@ -849,19 +850,9 @@
                         </div>
                         <!-- /.item -->
 
-                        <div class="item" style="background-image: url({{asset('frontend/assets/images/sliders/02.jpg')}});">
-                            <div class="container-fluid">
-                                <div class="caption bg-color vertical-center text-left">
-                                    <div class="slider-header fadeInDown-1">Spring 2016</div>
-                                    <div class="big-text fadeInDown-1"> Women <span class="highlight">Fashion</span> </div>
-                                    <div class="excerpt fadeInDown-2 hidden-xs"> <span>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit</span> </div>
-                                    <div class="button-holder fadeInDown-3"> <a href="index.php?page=single-product" class="btn-lg btn btn-uppercase btn-primary shop-now-button">Shop Now</a> </div>
-                                </div>
-                                <!-- /.caption -->
-                            </div>
-                            <!-- /.container-fluid -->
-                        </div>
+
                         <!-- /.item -->
+                        @endforeach
 
                     </div>
                     <!-- /.owl-carousel -->
