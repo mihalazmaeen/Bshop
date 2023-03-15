82e41ee435d3a93863cb57a3f681068e1e0d7aa1
mihalazmaeen <azmaeenmihal1@gmail.com>
2023-03-15 16:16:18 +0600

    mailing after successful order

--- app/Http/Controllers/User/StripeController.php
+++ app/Http/Controllers/User/StripeController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Mail\OrderMail;
 use App\Models\Order;
+
 use App\Models\OrderItem;
 use Carbon\Carbon;
 use Gloudemans\Shoppingcart\Facades\Cart;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Session;
 
 class StripeController extends Controller
@@ -58,6 +61,14 @@ class StripeController extends Controller
             'created_at'=>Carbon::now(),
 
         ]);
+        $invoice=Order::findOrFail($order_id);
+        $data=[
+            'invoice_no'=>$invoice->invoice_no,
+            'amount'=>$total_amount,
+            'name'=>$invoice->name,
+            'email'=>$invoice->email,
+        ];
+        Mail::to($request->email)->send(new OrderMail($data));
         $carts=Cart::content();
         foreach ($carts as $cart){
             OrderItem::insert([
