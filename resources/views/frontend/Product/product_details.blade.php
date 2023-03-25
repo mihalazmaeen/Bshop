a1953a729dfb734331839e57ff65e0a2a88cc6fd
mihalazmaeen <azmaeenmihal1@gmail.com>
2023-03-26 02:32:34 +0600

    follow on social media

--- resources/views/frontend/Product/product_details.blade.php
+++ resources/views/frontend/Product/product_details.blade.php
@@ -267,6 +267,9 @@
 
 
 
+                                    <!-- Go to www.addthis.com/dashboard to customize your tools -->
+                                    <div class="addthis_inline_follow_toolbox"></div>
+
 
 
 
@@ -543,5 +546,8 @@
             </div><!-- /.row -->
 
         </div>
+        <!-- Go to www.addthis.com/dashboard to customize your tools -->
+        <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-641f592e08c81a58"></script>
+
 
 @endsection
