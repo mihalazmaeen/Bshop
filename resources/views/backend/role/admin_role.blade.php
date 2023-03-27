359007c21ff8a52f8408c86f7341023013dc8d93
mihalazmaeen <azmaeenmihal1@gmail.com>
2023-03-27 15:50:39 +0600

    admin priviliges show

--- resources/views/backend/role/admin_role.blade.php
+++ resources/views/backend/role/admin_role.blade.php
@@ -39,12 +39,79 @@
                                             <td>{{$item->name}}</td>
                                             <td>{{$item->email}}</td>
 
-                                           <td>access</td>
+                                           <td>
+                                               @if($item->brand==1)
+                                                   <span class="badge btn-primary">brand</span>
+                                               @else
+                                               @endif
+                                               @if($item->brand==1)
+                                                   <span class="badge btn-primary">brand</span>
+                                               @else
+                                               @endif
+                                               @if($item->category==1)
+                                                   <span class="badge btn-primary">category</span>
+                                               @else
+                                               @endif
+                                               @if($item->product==1)
+                                                   <span class="badge btn-primary">product</span>
+                                               @else
+                                               @endif
+                                               @if($item->slider==1)
+                                                   <span class="badge btn-primary">slider</span>
+                                               @else
+                                               @endif
+                                               @if($item->coupons==1)
+                                                   <span class="badge btn-primary">coupons</span>
+                                               @else
+                                               @endif
+                                               @if($item->blog==1)
+                                                   <span class="badge btn-primary">blog</span>
+                                               @else
+                                               @endif
+                                               @if($item->shipping==1)
+                                                   <span class="badge btn-primary">shipping</span>
+                                               @else
+                                               @endif
+                                               @if($item->setting==1)
+                                                   <span class="badge btn-primary">setting</span>
+                                               @else
+                                               @endif
+                                               @if($item->returnorder==1)
+                                                   <span class="badge btn-primary">returnorder</span>
+                                               @else
+                                               @endif
+                                               @if($item->review==1)
+                                                   <span class="badge btn-primary">review</span>
+                                               @else
+                                               @endif
+                                               @if($item->orders==1)
+                                                   <span class="badge btn-primary">orders</span>
+                                               @else
+                                               @endif
+                                               @if($item->stock==1)
+                                                   <span class="badge btn-primary">stock</span>
+                                               @else
+                                               @endif
+                                               @if($item->reports==1)
+                                                   <span class="badge btn-primary">reports</span>
+                                               @else
+                                               @endif
+                                               @if($item->alluser==1)
+                                                   <span class="badge btn-primary">alluser</span>
+                                               @else
+                                               @endif
+                                               @if($item->adminuserrole==1)
+                                                   <span class="badge btn-primary">adminuserrole</span>
+                                               @else
+                                               @endif
+
+
+                                           </td>
 
 
                                             <td width="30%">
-                                                <a href="{{route('pending-order-details',$item->id)}}" class="btn btn-info" title="Edit Category"><i class="fa fa-eye"></i></a>
-                                                <a href="{{route('admin.invoice',$item->id)}}" id="" class="btn btn-danger" title="Download invoice"><i class="fa fa-download"></i></a>
+                                                <a href="{{route('pending-order-details',$item->id)}}" class="btn btn-info" title="Edit Admin"><i class="fa fa-eye"></i></a>
+                                                <a href="{{route('admin.invoice',$item->id)}}" id="delete" class="btn btn-danger" title="Delete Admin"><i class="fa fa-trash"></i></a>
                                             </td>
 
                                         </tr>
